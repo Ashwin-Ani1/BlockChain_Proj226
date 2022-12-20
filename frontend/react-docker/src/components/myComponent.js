979b7c1e1b8684882584ffb5787dc90c1208e3da
vanessali824 <108141117+vanessali824@users.noreply.github.com>
2022-12-20 15:31:50 -0500

    Update myComponent.js

--- frontend/react-docker/src/components/myComponent.js
+++ frontend/react-docker/src/components/myComponent.js
@@ -2,7 +2,10 @@ import React, { useState, useEffect } from 'react';
 
 export default function MyComponent(props){
     const [data, setData] = useState(null);
-    let start=props.starttime;
+    let start=null;
+    let end=null;
+
+    start=props.starttime;
     let yyyy = start.getFullYear();
     let mm = String(start.getMonth() + 1).padStart(2, '0'); //January is 0!
     let dd = String(start.getDate()).padStart(2, '0');
@@ -10,7 +13,7 @@ export default function MyComponent(props){
     console.log(start);
 
 
-    let end = props.endtime;
+    end=props.endtime;
     yyyy = end.getFullYear();
     mm = String(end.getMonth() + 1).padStart(2, '0'); //January is 0!
     dd = String(end.getDate()).padStart(2, '0');
@@ -19,19 +22,31 @@ export default function MyComponent(props){
 
      useEffect(() => {
 
-     fetch("http://localhost/api/read.php?format=geojson&starttime="+start+"&endtime="+ end)
+     fetch("http://localhost/api/read.php?format=geojson&starttime="+start+"&endtime="+ end +"&minmagnitude=5")
             .then(response => response.json())
             .then(data => setData(data));
 
-    }, []);
+    },  [props.starttime, props.endtime]);
     console.log(data);
     // render the component using the data from the API
-    return <div>{JSON.stringify(data)}</div>;
+    let print='Earthquakes from '+start+' to '+end+' with a minimum magnitude of 5';
+    if(data==null){
+        return <div>{print} {data}</div>;
+    }
+    else {
+        let element=[]
+        for (let i = 0; i < data.features.length; i++) {
+            element[i]= <div>{data.features[i].properties.title}</div>;
+        }
+        return (
+            <>
+                {print}
+                {element}
+            </>
+        );
+    }
+
 
 }
-
-
-
-
 
 
