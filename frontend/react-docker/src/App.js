118c89a12ba96a91e7cd3b96a4158cd6ca8761dc
vanessali824 <108141117+vanessali824@users.noreply.github.com>
2022-12-20 17:56:07 -0500

    Update App.js

--- frontend/react-docker/src/App.js
+++ frontend/react-docker/src/App.js
@@ -4,6 +4,7 @@ import React, {useState} from "react";
 import DatePicker from "react-datepicker";
 import InputNumber from 'react-input-number';
 import "react-datepicker/dist/react-datepicker.css";
+import CheckboxList from "./components/EmergencyKit";
 
 function App() {
     const [startDate, setStartDate] = useState(new Date());
@@ -21,12 +22,19 @@ function App() {
             <DatePicker dateFormat="yyyy-MM-dd" selected={endDate} onChange={(date) => setEndDate(date)} />
             <p>Please choose a minimum magnitude</p>
             <InputNumber min={0} max={10} value={minMagnitude} onChange={setMinMagnitude} />
-            <p></p>
-            <MyComponent starttime={startDate} endtime={endDate} minMagnitude={minMagnitude}/>
-
-            <h2 style={{textDecoration: 'underline'}}>Preparing an Earthquake Kit:</h2>
-
-
+            <p style={{color: 'white', fontWeight: 'bold'}}>
+                <MyComponent starttime={startDate} endtime={endDate} minMagnitude={minMagnitude}/>
+            </p>
+            <p style={{color: 'white'}}>
+                Visit the <a href="https://www.cdc.gov/disasters/earthquakes/during.html#print">CDC website</a> for more information on what to do in the event of an earthquake.
+            </p>
+            <h2 style={{textDecoration: 'underline'}}>BUILDING AN EMERGENCY PREPAREDNESS KIT</h2>
+            <p>
+                Being prepared means being equipped with the proper supplies you may need in the event of an emergency or disaster. Keep your supplies in an easy-to-carry emergency preparedness kit that you can use at home or take with you in case you must evacuate. At a minimum, you should have the basic supplies listed below:
+            </p>
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <CheckboxList />
+            </div>
 
         </div>
     );
